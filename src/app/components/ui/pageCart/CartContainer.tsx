53815c091aa2d091f8110b3dd57f3636b0658ba2
RaulZBergamo <raulbergamo416@gmail.com>
2025-01-26 17:11:01 -0300

    feat: Enhance CartContainer loading state management and improve UI responsiveness

--- src/app/components/ui/pageCart/CartContainer.tsx
+++ src/app/components/ui/pageCart/CartContainer.tsx
@@ -143,59 +143,62 @@ const ItensCart: React.FC<{ openCart: boolean, setOpenCart: React.Dispatch<React
             return;
         }
         router.push('/pages/checkout');
-    }
-
-    if (loading) {
-        return (
-            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-                <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-20 w-20"></div>
-            </div>
-        )
-    }
+    };
 
     return (
         <>
-            <div className="fixed rounded-full bottom-2 right-2 py-3 px-3 bg-red-700 opacity-90 hover:opacity-100 z-50 cursor-pointer">
-                <IoMdCart size={24} color="white" onClick={() => setOpenCart(!openCart)} />
-            </div>
-            {openCart && (
-                <div className="z-50">
-                    <div className="fixed bottom-16 right-3 bg-white px-6 py-4 z-50 rounded-lg shadow-lg w-80">
-                        <div className="grid grid-cols-1 gap-4">
-                            {cartItems.length > 0 ? cartItems.map((item) => (
-                                <div key={item.id} className="flex items-center justify-between">
-                                    <div className="flex items-center space-x-4">
-                                        <h1 className="text-gray-800 font-medium">{item.name}</h1>
-                                    </div>
-                                    <div className="flex items-center space-x-2">
-                                        <button onClick={() => decrementQuantity(item.id)} className="bg-gray-200 rounded p-1 hover:bg-gray-300">
-                                            <IoMdRemove />
-                                        </button>
-                                        <p className="text-gray-800">{item.quantity}</p>
-                                        <button onClick={() => incrementQuantity(item.id)} className="bg-gray-200 rounded p-1 hover:bg-gray-300">
-                                            <IoMdAdd />
-                                        </button>
-                                    </div>
-                                    <p className="text-gray-800 font-medium">{(item.price * (item.quantity ?? 1)).toFixed(2)}</p>
-                                    <button onClick={() => removeItem(item.id)} className="bg-red-500 text-white p-1 rounded hover:bg-red-600">
-                                        <IoMdTrash />
-                                    </button>
-                                </div>
-                            )) : (
-                                <p className="text-gray-500">O carrinho está vazio.</p>
-                            )}
-                        </div>
-                        <div className="grid gap-4 pt-4 border-t border-gray-200">
-                            <p className="font-bold text-right text-lg">Total: R${calculateTotal().toFixed(2)}</p>
-                            <button onClick={handlePayment} className="px-4 py-2 bg-red-700 rounded text-white w-full hover:bg-red-800">PAGAMENTO</button>
-                        </div>
+            {loading && (
+                <>
+                    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                        <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-20 w-20"></div>
                     </div>
-                    <div onClick={() => setOpenCart(false)} className="bg-gray-700 opacity-30 fixed top-0 w-full h-full z-0"></div>
-                </div>
+                    <div className="fixed inset-0 z-40"></div>
+                </>
             )}
+            <div className={`relative ${loading ? 'opacity-50 pointer-events-none' : 'opacity-100'}`}>
+                <div className="fixed rounded-full bottom-2 right-2 py-3 px-3 bg-red-700 opacity-90 hover:opacity-100 z-50 cursor-pointer">
+                    <IoMdCart size={24} color="white" onClick={() => setOpenCart(!openCart)} />
+                </div>
+                {openCart && (
+                    <div className="z-50">
+                        <div className="fixed bottom-16 right-3 bg-white px-6 py-4 z-50 rounded-lg shadow-lg w-80">
+                            <div className="grid grid-cols-1 gap-4">
+                                {cartItems.length > 0 ? cartItems.map((item) => (
+                                    <div key={item.id} className="flex items-center justify-between">
+                                        <div className="flex items-center space-x-4">
+                                            <h1 className="text-gray-800 font-medium">{item.name}</h1>
+                                        </div>
+                                        <div className="flex items-center space-x-2">
+                                            <button onClick={() => decrementQuantity(item.id)} className="bg-gray-200 rounded p-1 hover:bg-gray-300">
+                                                <IoMdRemove />
+                                            </button>
+                                            <p className="text-gray-800">{item.quantity}</p>
+                                            <button onClick={() => incrementQuantity(item.id)} className="bg-gray-200 rounded p-1 hover:bg-gray-300">
+                                                <IoMdAdd />
+                                            </button>
+                                        </div>
+                                        <p className="text-gray-800 font-medium">{(item.price * (item.quantity ?? 1)).toFixed(2)}</p>
+                                        <button onClick={() => removeItem(item.id)} className="bg-red-500 text-white p-1 rounded hover:bg-red-600">
+                                            <IoMdTrash />
+                                        </button>
+                                    </div>
+                                )) : (
+                                    <p className="text-gray-500">O carrinho está vazio.</p>
+                                )}
+                            </div>
+                            <div className="grid gap-4 pt-4 border-t border-gray-200">
+                                <p className="font-bold text-right text-lg">Total: R${calculateTotal().toFixed(2)}</p>
+                                <button onClick={handlePayment} className="px-4 py-2 bg-red-700 rounded text-white w-full hover:bg-red-800">PAGAMENTO</button>
+                            </div>
+                        </div>
+                        <div onClick={() => setOpenCart(false)} className="bg-gray-700 opacity-30 fixed top-0 w-full h-full z-0"></div>
+                    </div>
+                )}
+            </div>
         </>
     );
 };
+
 
 const CartContainer: React.FC = () => {
     const [openCart, setOpenCart] = useState(false);
@@ -206,7 +209,10 @@ const CartContainer: React.FC = () => {
     const addToCart = (item: Item) => {
         setLoading(true);
 
-        if (transaction === undefined) return;
+        if (transaction === undefined) {
+            setLoading(false);
+            return;
+        }
 
         if (!transaction) {
             createTransaction({ cart: [item] });
@@ -236,10 +242,10 @@ const CartContainer: React.FC = () => {
                     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                         <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-20 w-20"></div>
                     </div>
-                    <div className="fixed inset-0 z-40"></div> {/* Adicione esta linha */}
+                    <div className="fixed inset-0 z-40"></div>
                 </>
             )}
-            <div className={`${loading ? 'opacity-50' : 'opacity-100'}`}>
+            <div className={`${loading ? 'opacity-50 pointer-events-none' : 'opacity-100'}`}>
                 <div className="mb-4">
                     <input
                         type="text"
