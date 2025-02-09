82a4c786446ca90bdba5b216c7c5e29098ff9fb3
RaulZBergamo <raulbergamo416@gmail.com>
2025-02-09 18:47:58 -0300

    feat: Update import path for ProfilePage component and set baseUrl in tsconfig

--- src/app/pages/profile/page.tsx
+++ src/app/pages/profile/page.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import ProfilePage from '../../components/ui/profilePage/NewProfilePage';
+import { ProfilePage } from '@/app/components/ui/profilePage/NewProfilePage';
 
 function Profile() {
     return ( 
