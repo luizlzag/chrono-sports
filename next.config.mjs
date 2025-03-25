47c31e23523c60e0f0b1d8355e30ca9331fc3a6f
RaulZBergamo <raulbergamo416@gmail.com>
2025-03-25 20:47:17 -0300

    feat: Add Capacitor configuration and update Next.js settings for export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
       domains: ['cdn.awsli.com.br', 'i.postimg.cc', 'img.freepik.com'], // Adicione os domínios que você quer permitir
     },
